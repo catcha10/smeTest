9f30a5dc487fedfcb2cffb6e0abc5e7f8cbb0e3b
catcha10 <chacatherine@gmail.com>
2023-05-19 10:59:58 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Nav from "../components/Nav";
-import TextFilter from '@/components/TextFilter';
+import TextFilter from '../components/TextFilter';
 
 const inter = Inter({ subsets: ['latin'] })
 
